97e9c3dde35ff62ac5226c74dee1e6c948f13117
Colby Blaisdell <ccblaisdell@gmail.com>
2015-09-29 21:10:20 -0500

    Add users cache to avoid repetitive lookups

--- lib/dota_quantify_elixir.ex
+++ lib/dota_quantify_elixir.ex
@@ -13,6 +13,7 @@ defmodule DotaQuantify do
       worker(DotaQuantify.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(DotaQuantify.Worker, [arg1, arg2, arg3]),
+      worker(DotaQuantify.MyUsers, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
---
+++ lib/dota_quantify_elixir/my_users.ex
@@ -0,0 +1,51 @@
+defmodule DotaQuantify.MyUsers do
+	alias DotaQuantify.Repo
+	use GenServer
+
+	## Client API
+
+	def start_link(opts \\ []) do
+		GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+	end
+
+	def all() do
+		GenServer.call(__MODULE__, :all)
+	end
+
+	def get(id) do
+		GenServer.call(__MODULE__, {:get, id})
+	end
+
+	def exists?(id) do
+		GenServer.call(__MODULE__, {:exists?, id})
+	end
+
+	## Server Callbacks
+
+	def init(:ok) do
+		users = DotaQuantify.User 
+		|> Repo.all
+		|> Enum.map(fn user -> {user.dotaid, %{personaname: user.personaname, avatar: user.avatar}} end)
+		|> Enum.into(%{})
+		{:ok, users}
+	end
+
+	def handle_call(:all, _from, users) do
+		{:reply, {:ok, users}, users}
+	end
+
+	def handle_call({:get, id}, _from, users) do
+		case Map.fetch(users, id) do
+			{:ok, user} -> {:reply, {:ok, user}, users}
+			_ -> {:reply, {:error}, users}
+		end
+	end
+
+	def handle_call({:exists?, id}, _from, users) do
+		case Map.fetch(users, id) do
+			{:ok, _user} -> {:reply, {:ok, true}, users}
+			_ -> {:reply, {:ok, false}, users}
+		end
+	end
+
+end
