45dee6c37b56c791799694fa68d79478464a204d
Colby Blaisdell <ccblaisdell@gmail.com>
2015-10-09 18:32:41 -0500

    Add hero and item images

--- web/views/player_view.ex
+++ web/views/player_view.ex
@@ -1,3 +1,19 @@
 defmodule Dotes.PlayerView do
   use Dotes.Web, :view
+
+  def item_img(id, opts \\ []) do
+    case Dota.Item.get(id) do
+      nil -> nil
+      item ->
+        tag :img, [{:src, "/images/items/#{item["name"]}_lg.png"} | opts]
+    end
+  end
+
+  def hero_img(id, size \\ "sb.png") do
+    case Dota.Hero.get(id) do
+      nil -> nil
+      hero ->
+        tag :img, src: "/images/heroes/#{hero["name"]}_#{size}"
+    end
+  end
 end
